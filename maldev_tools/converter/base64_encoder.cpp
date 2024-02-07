9261bf49ed893adeb1bb3271f89b3d11367f4034
vincgonzo <vincgonzo@hotmail.fr>
2024-02-07 15:06:19 +0100

    fix(0.1): refactor & add base64 converter

---
+++ maldev_tools/converter/base64_encoder.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+std::vector<uint8_t> xor_encrypt(const std::vector<uint8_t>& shell, const std::vector<uint8_t>& keyBytes) {
+    std::vector<uint8_t> encrypted(shell.size());
+
+    for (size_t i = 0; i < shell.size(); ++i) {
+        encrypted[i] = shell[i] ^ keyBytes[i % keyBytes.size()];
+    }
+
+    return encrypted;
+}
+
+int main() {
+    // XOR Key - It has to be the same in the Dropper for Decrypting
+    std::string key = "THMK3y123!";
+
+    // Convert Key into bytes
+    std::vector<uint8_t> keyBytes(key.begin(), key.end());
+
+    // Original Shellcode here (C++ format)
+    std::vector<uint8_t> buf = { // [shellcode] 
+    };
+
+    // XORing byte by byte and saving into a new vector of bytes
+    std::vector<uint8_t> encoded = xor_encrypt(buf, keyBytes);
+
+    // Convert the encoded vector to a Base64 string
+    std::string base64Encoded(encoded.begin(), encoded.end());
+    std::cout << base64Encoded << std::endl;
+
+    return 0;
+}
